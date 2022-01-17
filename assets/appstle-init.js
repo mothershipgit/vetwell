8196e1a0ee4424195fa4875b41cccdcca0f05c75
Nicolas Telfer <nico@spoke-london.com>
2022-01-17 19:20:09 +0100

    Adds initial theme files

---
+++ assets/appstle-init.js
@@ -0,0 +1,138 @@
+(function (window, k) {
+    if (!window.AppstleIncluded && (!urlIsProductPage() || 'V1' === 'V2')) {
+      window.AppstleIncluded = true;
+      appstleLoadScript = function (src, callback) {
+        var script = document.createElement("script");
+        script.charset = "utf-8";
+        script.async = true;
+        script.src = src;
+        script.onload = script.onreadystatechange = function () {
+          if (!script.readyState || /loaded|complete/.test(script.readyState)) {
+            script.onload = script.onreadystatechange = null;
+            script = k;
+            callback && callback();
+          }
+        };
+        document.getElementsByTagName("head")[0].appendChild(script)
+      };
+      appstleLoadScript("https://cdn.shopify.com/s/files/1/0611/4770/9678/t/3/assets/appstle-subscription.js?v=1641892726");
+
+      window.RS = Window.RS || {};
+      RS.Config = {
+        "selectors": {
+            "payment_button_selectors": "form[action$='/cart/add'] .shopify-payment-button",
+            "subscriptionLinkSelector": "div.grid.myaccount",
+            "atcButtonPlacement": "BEFORE",
+            "subscriptionLinkPlacement": "BEFORE",
+        },
+        "useUrlWithCustomerId": false,
+        "atcButtonSelector": "div.product-form__buttons",
+        "moneyFormat": "{% raw %}\u00A3{{amount}}{% endraw %}",
+        "oneTimePurchaseText": "One Time Purchase",
+        "shop": "vetwell-scientific-dev.myshopify.com",
+        "deliveryText": "delivery",
+        "purchaseOptionsText": "Purchase Options",
+        "manageSubscriptionButtonText": "Manage Subscription",
+        "subscriptionOptionText": "Subscribe and save",
+        "sellingPlanSelectTitle": "Delivery Frequency",
+        "subscriptionPriceDisplayText": "",
+        "tooltipTitle": "Subscription detail",
+        "api_key": "NGKB1qU5kKnj",
+        "showTooltipOnClick": "false",
+        "tooltipDesctiption": "<strong>Have complete control of your subscriptions<\/strong><br\/><br\/>Skip, reschedule, edit, cancel deliveries anytime matching your needs.",
+        "tooltipDescriptionOnPrepaidPlan": "<b>Prepaid Plan Details<\/b><\/br> Total price: {{totalPrice}} ( Price for every delivery: {{pricePerDelivery}})",
+        "tooltipDescriptionOnMultipleDiscount": "<b>Discount Details<\/b><\/br> Initial discount is {{discountOne}} and then {{discountTwo}}",
+        "tooltipDescriptionCustomization": "{{{defaultTooltipDescription}}} <\/br>  {{{prepaidDetails}}} <\/br> {{{discountDetails}}}",
+        "orderStatusManageSubscriptionTitle": "Subscription",
+        "orderStatusManageSubscriptionDescription": "Continue to your account to view and manage your subscriptions. Please use the same email address that you used to buy the subscription.",
+        "orderStatusManageSubscriptionButtonText": "Manage your subscription",
+        "subscriptionOptionSelectedByDefault" : false,
+        "totalPricePerDeliveryText" : "{{prepaidPerDeliveryPrice}}\/delivery",
+        "memberOnlySellingPlansJson": {},
+        "nonMemberOnlySellingPlansJson": {},
+        "widgetEnabled": true,
+        "showTooltip" : true,
+        "sortByDefaultSequence": false,
+        "showSubOptionBeforeOneTime": false,
+        "showStaticTooltip": false,
+        "showAppstleLink": false,
+        "sellingPlanTitleText" : "{{sellingPlanName}} - {{sellingPlanPrice}}",
+        "oneTimePriceText" : "{{price}}",
+        "selectedPayAsYouGoSellingPlanPriceText" : "{{price}}",
+        "selectedPrepaidSellingPlanPriceText" : "{{pricePerDelivery}}",
+        "selectedDiscountFormat" : "SAVE {{selectedDiscountPercentage}}",
+        "manageSubscriptionBtnFormat" : "<a href='apps\/subscriptions' class='appstle_manageSubBtn' ><button class='btn' style='padding: 2px 20px'>Manage Subscription<\/button><a><br><br>",
+        "manageSubscriptionUrl" : "apps\/subscriptions",
+        "appstlePlanId": 1,
+        "showCheckoutSubscriptionBtn": true,
+        "disableLoadingJquery": false,
+        "css": {
+            "appstle_subscription_widget": {
+                "margin-top": "" ,
+                "margin-bottom": "",
+            },
+
+            "appstle_subscription_wrapper": {
+                "border-width": "",
+                "border-color": "",
+            },
+
+            "appstle_circle": {
+                "border-color": "",
+            },
+
+            "appstle_dot": {
+                "background-color": "",
+            },
+
+            "appstle_select": {
+                "padding-top": "5px",
+                "padding-bottom": "5px",
+                "padding-left": "2px",
+                "padding-right": "2px",
+                "border-width": "",
+                "border-style": "",
+                "border-color": "",
+                "border-radius": "",
+            },
+
+            "tooltip_subscription_svg": {
+                "fill": "",
+            },
+
+            "appstle_tooltip": {
+                "color": "",
+                "background-color": "",
+            },
+
+            "appstle_tooltip_border_top_color": {
+                "border-top-color": "",
+            },
+
+            "appstle_subscription_final_price": {
+                "color": "#d11010",
+            },
+            "appstle_widget_text_color": {
+                "color": "",
+            },
+            "appstle_selected_background": {
+                "background": "transparent",
+            },
+            "customCSS": "#appstle_subscription_widget0 .appstle_subscription_wrapper .appstle_select {\ncolor: inherit;\n}\n.price {\ndisplay: inline-block!important;\n}",
+            "customerPortalCss": "",
+            "priceSelector": "",
+            "landingPagePriceSelector": "",
+            "quickViewClickSelector": "",
+            "badgeTop": "",
+            "pricePlacement": "BEFORE",
+        }
+      };
+
+    }
+
+    function urlIsProductPage() {
+    // return null != decodeURIComponent(window.location.pathname).match(/\/products\/(([a-zA-Z0-9]|[\-\.\_\~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[\ud83c\ud83d\ud83e][\ud000-\udfff]){1,})\/?/)
+    return decodeURIComponent(window.location.pathname).includes('/products/');
+    }
+  }
+)(window);
